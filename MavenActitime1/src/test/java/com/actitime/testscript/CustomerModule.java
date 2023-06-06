047fa0f69be59c363166eb13f6204db60f91611a
DELL <DELL@DESKTOP-EFLV044>
2023-06-07 00:21:49 +0530

    kk

--- MavenActitime1/src/test/java/com/actitime/testscript/CustomerModule.java
+++ MavenActitime1/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -9,15 +9,10 @@ import com.zoho.generic.BaseClass;
 import com.zoho.generic.FileLib;
 
 public class CustomerModule {
-	static {
-		System.setProperty("webdriver.chrome.driver", "./src/main/resources/driver/chromedriver.exe");
-
-	}
 	@Test
    public void createCustomer() {
 	Reporter.log("createCampaign");	
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://demo.actitime.com");
+	
 	
    }
 }
